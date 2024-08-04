8eb6eaf524c2e756b30306a0f4916e63c3899e56
7FOX7 <kheeugene@gmail.com>
2024-08-04 17:10:54 -0400

    added a styling for the 'login' button

--- src/components/Login/BottomSection.jsx
+++ src/components/Login/BottomSection.jsx
@@ -1,7 +1,7 @@
 import { Box, Button, useMediaQuery, Typography } from "@mui/material"
 import theme from "../../theme/theme"
 
-function BottomSection({displayerColor, fillness, passwordMessage, onClick}) {
+function BottomSection({displayerColor, fillness, passwordMessage, onClick, formComplete, displayerVisibility}) {
   const smallScreen = useMediaQuery(theme.breakpoints.down('md'));  
 
   return (
@@ -14,7 +14,8 @@ function BottomSection({displayerColor, fillness, passwordMessage, onClick}) {
         width: "100%",  
         border: "1px solid rgb(50, 50, 50)", 
         borderRadius: "5px", 
-        height: "10px"
+        height: "10px", 
+        visibility: displayerVisibility
     }}>
           <Box sx={{
               width: fillness,  
@@ -32,9 +33,30 @@ function BottomSection({displayerColor, fillness, passwordMessage, onClick}) {
       }}>
         <Typography color={displayerColor}>{passwordMessage}</Typography>
       </Box>
-      <Button onClick={onClick} type="submit" sx={{ 
+      <Button onClick={onClick} type="submit" disabled={!formComplete} sx={{ 
         marginTop: "25px", 
-        background: "red"
+        background: "#000", 
+        borderRadius: "50px",
+        transition: "0.5s", 
+        ":hover": {
+          background: "rgb(80, 80, 80)",
+        }, 
+        "&:after": {
+          content: `${!formComplete ? "none" : "''"}`, 
+          borderRadius: "50px",
+          width: "105%", 
+          height: "130%", 
+          position: "absolute", 
+          left: "5",  
+          top: "5", 
+          background: "linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)", 
+          zIndex: "-1"
+        }, 
+        ":disabled": {
+          background: "rgba(0, 0, 0, 0.15)",
+          cursor: "not-allowed", 
+          border: "none", 
+        }
       }}>
         Login
       </Button>
--- src/components/Login/InputSection.jsx
+++ src/components/Login/InputSection.jsx
@@ -3,23 +3,25 @@ import { Box, Typography } from "@mui/material"
 import CustomInput from "../../customs/CustomInput"
 import BottomSection from "./BottomSection"
 import { strongRegex, mediumRegex } from "../../data/passwordRejex"
-import { Navigate } from "react-router-dom"
 
 function InputSection() {
   const usernameRef = useRef(null); 
   const passwordRef = useRef(null);
   const errorMessageRef = useRef(null); 
   const [displayerColor, setDisplayerColor] = useState(null); 
+  const [displayerVisibility, setDisplayerVisibility] = useState("hidden"); 
   const [fillness, setFillness] = useState(null);
   const [passwordMessage, setPasswordMessage] = useState(null);  
+  const [formComplete, setFormComplete] = useState(false); 
 
   function handleSubmit(e) {
     e.preventDefault(); 
-    
   }
 
   function checkForUsername() {
     usernameRef.current.value === "" ? errorMessageRef.current.style.visibility = "visible" : errorMessageRef.current.style.visibility = "hidden"
+
+    strongRegex.test(passwordRef.current.value) && usernameRef.current.value !== "" ? setFormComplete(true) : setFormComplete(false);
   }
 
   function checkForPasswordStrength() {
@@ -38,6 +40,9 @@ function InputSection() {
       setFillness("20%")
       setPasswordMessage("The password is easy to guess. Please, include at least 1 uppercase letter, 1 number, and 1 special character.")
     }
+
+    setDisplayerVisibility("visible"); 
+    strongRegex.test(passwordRef.current.value) && usernameRef.current.value !== "" ? setFormComplete(true) : setFormComplete(false);
   }
 
   return (
@@ -54,7 +59,7 @@ function InputSection() {
         </Box>
         <CustomInput placeholder="Enter username" onChange={checkForUsername} ref={usernameRef} />
         <CustomInput placeholder="Enter password" onChange={checkForPasswordStrength} ref={passwordRef} />
-        <BottomSection displayerColor={displayerColor} fillness={fillness} passwordMessage={passwordMessage} onClick={handleSubmit}/>
+        <BottomSection displayerColor={displayerColor} fillness={fillness} passwordMessage={passwordMessage} onClick={handleSubmit} formComplete={formComplete} displayerVisibility={displayerVisibility} />
     </Box>
   )
 }
