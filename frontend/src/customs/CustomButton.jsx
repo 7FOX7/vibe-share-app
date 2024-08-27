f4f6242f871b9fb1b238c64a28f427984b4d8c06
7FOX7 <kheeugene@gmail.com>
2024-08-27 18:11:37 -0400

    Modified 'CustomButton' component to make it more reusable

--- frontend/src/customs/CustomButton.jsx
+++ frontend/src/customs/CustomButton.jsx
@@ -1,15 +1,15 @@
 import { useScreenSize } from "../contexts/ScreenSizeContext";
 import Box from "@mui/material/Box"; 
 
-const CustomButton = ({id, title, icon, backgroundColor, onClick}) => {
+const CustomButton = ({id, title, width="fit-content", border="1px solid", backgroundColor, fontSize, icon, onClick}) => {
     const {isSmallScreen} = useScreenSize(); 
     const buttonStyle = {
-        width: `${isSmallScreen ? "auto" : "fit-content"}`, 
+        width: width, 
         display: "flex",
         alignItems: "center", 
         justifyContent: "center", 
         backgroundColor: backgroundColor, 
-        border: "1px solid", 
+        border: border, 
         borderColor: "contrastColors.black.main", 
         color: "semiTransparentBlack.main", 
         fontFamily: "inherit", 
@@ -17,6 +17,7 @@ const CustomButton = ({id, title, icon, backgroundColor, onClick}) => {
         paddingInline: `${isSmallScreen ? "0" : "14px"}`, 
         flex: `${isSmallScreen ? "0.2 1 auto" : "none"}`,
         borderRadius: "50px",
+        fontSize: fontSize, 
         cursor: "pointer"
     }
 
