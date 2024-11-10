ce4cb78e4227dfb96ebfaceb209eaa6ccba34366
7FOX7 <kheeugene@gmail.com>
2024-11-09 23:13:11 -0500

    Removed a typo from the url string

--- frontend/axios.config.js
+++ frontend/axios.config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://vibe-share-app-project.onrender.com "
+const BASE_URL = "https://vibe-share-app-project.onrender.com"
 
 const _axios = axios.create({
    baseURL: BASE_URL,  
