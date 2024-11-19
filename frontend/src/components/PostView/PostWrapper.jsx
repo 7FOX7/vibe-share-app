ca3347f8f245acc578a9ba08f141530cd37fb7f9
7FOX7 <kheeugene@gmail.com>
2024-11-18 22:18:08 -0500

    Solved a bug when the username was not changing on when swiping posts

--- frontend/src/components/PostView/PostWrapper.jsx
+++ frontend/src/components/PostView/PostWrapper.jsx
@@ -5,6 +5,7 @@ import { useScreenSize } from "../../contexts/ScreenSizeContext";
 import { useSelectedButton } from "../../contexts/SelectedButtonContext";
 import Box from "@mui/material/Box";
 import Post from "./Post";
+import { usePostAuthor } from "../../contexts/PostAuthorContext";
 
 const PostWrapper = () => {
     const {posts, setPosts, geolocationFilteredPosts, setGeolocationFilteredPosts} = usePosts();
@@ -13,18 +14,17 @@ const PostWrapper = () => {
     const [index, setIndex] = useState(Number(id)); 
     const {isSmallScreen} = useScreenSize(); 
     const navigate = useNavigate(); 
+    const {setAuthor} = usePostAuthor()
 
     const currentPosts = selectedButton === "Local" ? geolocationFilteredPosts : posts
 
     const post = useMemo(() => {
         const post = currentPosts.find(_post => _post.id === index)
-        if(typeof(post) !== "undefined") {
-            return post
-        }
-        return
+        if(typeof(post) !== "undefined") return post
     }, [index, currentPosts])
 
     useEffect(() => {
+        console.log('I would run')
         navigate(`/post-view/${post.id}/${post.username}`, {relative: "route"})
     }, [index])
 
@@ -32,6 +32,7 @@ const PostWrapper = () => {
         const currentIndex = currentPosts.findIndex(_post => _post.id === index);
         if (currentIndex > 0) {
             setIndex(currentPosts[currentIndex - 1].id);
+            setAuthor(currentPosts[currentIndex - 1].username)
         }
     }, [index])
 
@@ -39,6 +40,7 @@ const PostWrapper = () => {
         const currentIndex = currentPosts.findIndex(_post => _post.id === index);
         if (currentIndex < currentPosts.length - 1) {
             setIndex(currentPosts[currentIndex + 1].id);
+            setAuthor(currentPosts[currentIndex + 1].username)
         }
     }, [index])
 
